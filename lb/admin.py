fcff7088ce0c462c51c718e23d637fd87c01460d
谭浩文 <tanhw2@yonghui.cn>
2021-12-21 09:56:54 +0800

    first commit

--- lb/admin.py
+++ lb/admin.py
@@ -1,3 +1,85 @@
 from django.contrib import admin
+from .models import LoadBalance, \
+    LoadBalanceHost, \
+    LoadBalanceMember, \
+    LoadBalanceListener, \
+    LoadBalancePath, \
+    SSL
 
-# Register your models here.
+
+@admin.register(LoadBalance)
+class LoadBalanceAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'name',
+                    'type',
+                    'ip',
+                    'subnet_id',
+                    'port_id',
+                    'provider',
+                    'status',
+                    'status_desc',
+                    'description',
+                    'tenant_id',
+                    'created_at')
+
+
+@admin.register(LoadBalanceHost)
+class LoadBalanceHostAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'listener_id',
+                    'host',
+                    'match_type',
+                    'created_at')
+
+
+@admin.register(LoadBalanceMember)
+class LoadBalanceMemberAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'p_id',
+                    'ip',
+                    'port',
+                    'weight',
+                    'created_at')
+
+
+@admin.register(LoadBalanceListener)
+class LoadBalanceListenerAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'lb_id',
+                    'protocol',
+                    'port',
+                    'type',
+                    'pip',
+                    'ssl_id',
+                    'algorithm',
+                    'status',
+                    'status_desc',
+                    'created_at')
+
+
+@admin.register(LoadBalancePath)
+class LoadBalancePathAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'host_id',
+                    'path',
+                    'algorithm',
+                    'match_type',
+                    'created_at')
+
+
+# @admin.register(SSL)
+# class SSLAdmin(admin.ModelAdmin):
+#     list_display = ('id',
+#                     'name'
+#                     'scope',
+#                     'cert_type',
+#                     'domain',
+#                     'cert',
+#                     'pkey',
+#                     'status',
+#                     'cert_begin_time',
+#                     'cert_end_time',
+#                     'update_time',
+#                     'tenant_id',
+#                     'tenant_name',
+#                     'created_at')
