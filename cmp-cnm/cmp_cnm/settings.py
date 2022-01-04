faca8ccd4ffee354212d89f23154782b16350c09
谭浩文 <tanhw2@yonghui.cn>
2022-01-04 20:24:59 +0800

    add static

--- cmp-cnm/cmp_cnm/settings.py
+++ cmp-cnm/cmp_cnm/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
+    'cmp_cnm',
     'nssrc',
     'lb',
     'lb.citrixapi'
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
