70202a1055be0e54a697b86646f08823f71931c3
thw <tanhaowen1996@gmail.com>
2022-08-11 09:19:13 +0800

    delete Lb 7

--- cmp-cnm/lb/serializers.py
+++ cmp-cnm/lb/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
-from .models import LoadBalance, LoadBalancePath, \
-    LoadBalanceMember, LoadBalanceListener, \
-    LoadBalanceHost, SSL
+from .models import LoadBalance, \
+    LoadBalanceMember, LoadBalanceListener
 
 
 class LoadBalanceSerializer(serializers.ModelSerializer):
-    tenant_id = serializers.CharField(required=False)
-    tenant_name = serializers.CharField(required=False)
     id = serializers.UUIDField(required=False)
     name = serializers.CharField(required=False)
     ip = serializers.CharField(required=False)
@@ -16,8 +13,9 @@ class LoadBalanceSerializer(serializers.ModelSerializer):
     port_id = serializers.UUIDField(required=False)
     provider = serializers.UUIDField(required=False)
     status = serializers.CharField(required=False)
-    status_desc = serializers.CharField(required=False)
     description = serializers.CharField(required=False)
+    tenant_id = serializers.CharField(required=False)
+    tenant_name = serializers.CharField(required=False)
 
     class Meta:
         model = LoadBalance
@@ -31,7 +29,6 @@ class LoadBalanceSerializer(serializers.ModelSerializer):
             'port_id',
             'provider',
             'status',
-            'status_desc',
             'description',
             'tenant_id',
             'tenant_name',
@@ -46,11 +43,8 @@ class LoadBalanceListenerSerializer(serializers.ModelSerializer):
     protocol = serializers.CharField(required=False)
     port = serializers.IntegerField(required=False)
     type = serializers.CharField(required=False)
-    pip = serializers.CharField(required=False)
-    ssl_id = serializers.UUIDField(required=False)
     algorithm = serializers.CharField(required=False)
     status = serializers.CharField(required=False)
-    status_desc = serializers.CharField(required=False)
 
     class Meta:
         model = LoadBalanceListener
@@ -61,55 +55,14 @@ class LoadBalanceListenerSerializer(serializers.ModelSerializer):
             'protocol',
             'port',
             'type',
-            'pip',
-            'ssl_id',
             'algorithm',
             'status',
-            'status_desc'
-        )
-
-
-class LoadBalanceHostSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(required=False)
-    name = serializers.CharField(required=False)
-    listener_id = serializers.UUIDField(required=False)
-    host = serializers.CharField(required=False)
-    match_type = serializers.CharField(required=False)
-
-    class Meta:
-        model = LoadBalanceHost
-        fields = (
-            'id',
-            'name',
-            'listener_id',
-            'host',
-            'match_type'
-        )
-
-
-class LoadBalancePathSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(required=False)
-    name = serializers.CharField(required=False)
-    host_id = serializers.UUIDField(required=False)
-    path = serializers.CharField(required=False)
-    algorithm = serializers.CharField(required=False)
-    match_type = serializers.CharField(required=False)
-
-    class Meta:
-        model = LoadBalancePath
-        fields = (
-            'id',
-            'name',
-            'host_id',
-            'path',
-            'algorithm',
-            'match_type'
         )
 
 
 class LoadBalanceMemberSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(required=False)
-    p_id = serializers.UUIDField(required=False)
+    listener_id = serializers.UUIDField(required=False)
     ip = serializers.CharField(required=False)
     port = serializers.IntegerField(required=False)
     weight = serializers.IntegerField(required=False)
@@ -118,40 +71,8 @@ class LoadBalanceMemberSerializer(serializers.ModelSerializer):
         model = LoadBalanceMember
         fields = (
             'id',
-            'p_id',
+            'listener_id',
             'ip',
             'port',
             'weight'
-        )
-
-
-class SSLSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(required=False)
-    name = serializers.CharField(required=False)
-    scope = serializers.CharField(required=False)
-    cert_type = serializers.CharField(required=False)
-    domain = serializers.CharField(required=False)
-    cert = serializers.CharField(required=False)
-    pkey = serializers.CharField(required=False)
-    status = serializers.CharField(required=False)
-    cert_begin_time = serializers.CharField(required=False)
-    cert_end_time = serializers.CharField(required=False)
-    tenant_id = serializers.UUIDField(required=False)
-    tenant_name = serializers.CharField(required=False)
-
-    class Meta:
-        model = SSL
-        fields = (
-            'id',
-            'name',
-            'scope',
-            'cert_type',
-            'domain',
-            'cert',
-            'pkey',
-            'status',
-            'cert_begin_time',
-            'cert_end_time',
-            'tenant_id',
-            'tenant_name'
         )
