a6ffc2968b6053a2190c8e0009296ff42d5b2c19
thw <tanhaowen1996@gmail.com>
2022-09-09 10:42:09 +0800

    fix bug

--- cmp-cnm/cmp_cnm/soloveCrossDomain.py
+++ cmp-cnm/cmp_cnm/soloveCrossDomain.py
@@ -5,4 +5,5 @@ class SoloveCrossDomainMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         response['Access-Control-Allow-Origin'] = "*"
         response['Access-Control-Allow-Headers'] = "Content-Type"
+        response['Access-Control-Allow-Methods'] = "GET,POST,DELETE,OPTIONS"
         return response
